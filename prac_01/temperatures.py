846e25827420e72d231f6be4ee6105d03dfe9f4b
qbouet <quentin.bouet@my.jcu.edu.au>
2023-09-23 13:37:01 +1000

    Refactor temperatures program to use functions

--- prac_01/temperatures.py
+++ prac_01/temperatures.py
@@ -3,6 +3,15 @@ CP1404/CP5632 - Practical
 Pseudocode for temperature conversion
 """
 
+
+def convert_celsius_to_fahrenheit():
+    return celsius * 9.0 / 5 + 32
+
+
+def convert_fahrenheit_to_celsius():
+    return 5 / 9 * (fahrenheit - 32)
+
+
 MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
@@ -11,12 +20,12 @@ choice = input(">>> ").upper()
 while choice != "Q":
     if choice == "C":
         celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print(f"Result: {fahrenheit:.2f} F")
+        fahrenheit = convert_celsius_to_fahrenheit()
+        print(f"Fahrenheit: {fahrenheit:.2f} F")
     elif choice == "F":
         fahrenheit = float(input("Fahrenheit: "))
-        celsius = 5 / 9 * (fahrenheit - 32)
-        print(f"Result: {celsius:.2f} F")
+        celsius = convert_fahrenheit_to_celsius()
+        print(f"Celsius: {celsius:.2f} C")
     else:
         print("Invalid option")
     print(MENU)
