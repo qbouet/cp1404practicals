db2ef72b6e69cfd9cdbf2208ababf24746c3828e
qbouet <quentin.bouet@my.jcu.edu.au>
2023-09-23 12:32:47 +1000

    Refactor password check program to use function

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,12 +1,20 @@
 
 def main():
     min_length = 4
+    password = get_password(min_length)
+    print_stars(password)
+
+
+def print_stars(password):
+    print(len(password) * '*')
+
+
+def get_password(min_length):
     password = str(input("Password: "))
     while len(password) < min_length:
         print("Password must be shorter than 4 characters")
         password = str(input("Password: "))
-
-    print(len(password) * '*')
+    return password
 
 
 main()
