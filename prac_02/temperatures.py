d270de318d8726698b415a6ee143522a8c6d4041
qbouet <quentin.bouet@my.jcu.edu.au>
2023-09-24 15:06:04 +1000

    Refactor main

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -4,30 +4,34 @@ Pseudocode for temperature conversion
 """
 
 
-def convert_celsius_to_fahrenheit():
+def main():
+    MENU = """C - Convert Celsius to Fahrenheit
+F - Convert Fahrenheit to Celsius
+Q - Quit"""
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            fahrenheit = convert_celsius_to_fahrenheit(celsius)
+            print(f"Fahrenheit: {fahrenheit:.2f} F")
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit: "))
+            celsius = convert_fahrenheit_to_celsius(fahrenheit)
+            print(f"Celsius: {celsius:.2f} C")
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+def convert_celsius_to_fahrenheit(celsius):
     return celsius * 9.0 / 5 + 32
 
 
-def convert_fahrenheit_to_celsius():
+def convert_fahrenheit_to_celsius(fahrenheit):
     return 5 / 9 * (fahrenheit - 32)
 
 
-MENU = """C - Convert Celsius to Fahrenheit
-F - Convert Fahrenheit to Celsius
-Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = convert_celsius_to_fahrenheit()
-        print(f"Fahrenheit: {fahrenheit:.2f} F")
-    elif choice == "F":
-        fahrenheit = float(input("Fahrenheit: "))
-        celsius = convert_fahrenheit_to_celsius()
-        print(f"Celsius: {celsius:.2f} C")
-    else:
-        print("Invalid option")
-    print(MENU)
-    choice = input(">>> ").upper()
-print("Thank you.")
+main()
