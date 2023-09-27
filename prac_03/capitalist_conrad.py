b0076b6b80df7968352a652f7edcff42025b3b09
qbouet <quentin.bouet@my.jcu.edu.au>
2023-09-27 15:05:44 +1000

    Update to tell how many days it simulated

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -14,9 +14,11 @@ MAX_DECREASE = 0.05  # 5%
 MIN_PRICE = 0.01
 MAX_PRICE = 1000.0
 INITIAL_PRICE = 10.0
+STARTING_DAY = 0
 
 price = INITIAL_PRICE
-print(f"${price:,.2f}")
+day = STARTING_DAY
+print(f"Starting price: ${price:,.2f}")
 
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0
@@ -30,6 +32,6 @@ while MIN_PRICE <= price <= MAX_PRICE:
         # generate a random floating-point number
         # between negative MAX_DECREASE and 0
         price_change = random.uniform(-MAX_DECREASE, 0)
-
+    day += 1
     price *= (1 + price_change)
-    print(f"${price:,.2f}")
+    print(f"On day {day} price is: {price:,.2f}")
