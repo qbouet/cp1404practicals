677581f2a9cdf4e8f982344300cc5140c54c688d
qbouet <quentin.bouet@my.jcu.edu.au>
2023-09-27 15:52:56 +1000

    Answer all the questions asked for this program

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -9,10 +9,25 @@ Answer the following questions:
 try:
     numerator = int(input("Enter the numerator: "))
     denominator = int(input("Enter the denominator: "))
+    # Check if the denominator is zero and keep asking for new denominator
+    while denominator == 0:
+        print("Denominator cannot be zero")
+        denominator = int(input("Enter the denominator: "))
     fraction = numerator / denominator
     print(fraction)
 except ValueError:
     print("Numerator and denominator must be valid numbers!")
+# This last except is no longer needed as error checking has been added to avoid a ZeroDivisionError
 except ZeroDivisionError:
     print("Cannot divide by zero!")
 print("Finished.")
+
+# 1. When will a ValueError occur?
+# A ValueError will occur when the user tries to input anything that isn't a number.
+
+# 2. When will a ZeroDivisionError occur?
+# A ZeroDivisionError will occur when the user inputs a zero as the denominator.
+
+# 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+# You could check if the denominator is equal to zero before executing the fraction.
+# This can be done by doing an error checking while loop for when the denominator is equal to zero.
