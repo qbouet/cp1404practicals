6a729f09347175579f5e2d0895fd61c6b10c739b
qbouet <quentin.bouet@my.jcu.edu.au>
2023-09-28 12:12:41 +1000

    Answer question 2 in files program

--- prac_03/files.py
+++ prac_03/files.py
@@ -6,3 +6,12 @@ name = input("Name: ")
 out_file = open(NAME_FILE, 'w')
 print(f"{name}", file=out_file)
 out_file.close()
+
+# 2. (In the same file, but as if it were a separate program)
+# Write code that opens "name.txt" and reads the name (as above) then prints,
+# "Your name is Bob" (or whatever the name is in the file).
+
+in_file = open(NAME_FILE, "r")
+name = in_file.read()
+print(f"Your name is {name}")
+in_file.close()
