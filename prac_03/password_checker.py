5c2e757b45edb97aae74b75363147021b2f65a5d
qbouet <quentin.bouet@my.jcu.edu.au>
2023-09-28 13:11:50 +1000

    Update so that if special characters are required, then check the count of those and return False if it's zero

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -5,7 +5,7 @@ Password checker "skeleton" code to help you get started
 
 MIN_LENGTH = 2
 MAX_LENGTH = 8
-SPECIAL_CHARS_REQUIRED = False
+SPECIAL_CHARS_REQUIRED = True
 SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
 
 
@@ -50,11 +50,14 @@ def is_valid_password(password):
             count_special += 1
 
     # if any of the 'normal' counts are zero, return False
-    if count_lower == 0 or count_upper == 0 or count_digit == 0 or count_special == 0:
+    if count_lower == 0 or count_upper == 0 or count_digit == 0:
         return False
 
-    # TODO: if special characters are required, then check the count of those
+    # if special characters are required, then check the count of those
     # and return False if it's zero
+    if SPECIAL_CHARS_REQUIRED:
+        if count_special == 0:
+            return False
 
     # if we get here (without returning False), then the password must be valid
     print(f"number of lowercase: {count_lower}")
