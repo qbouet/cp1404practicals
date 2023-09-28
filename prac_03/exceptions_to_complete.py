d7cf8c6b40c1c61cd45be33edf0e982349109e80
qbouet <quentin.bouet@my.jcu.edu.au>
2023-09-28 11:18:02 +1000

    Complete program

--- prac_03/exceptions_to_complete.py
+++ prac_03/exceptions_to_complete.py
@@ -7,7 +7,8 @@ is_finished = False
 while not is_finished:
     try:
         result = int(input("Enter a valid integer: "))
-        # TODO: this line
-    except:  # TODO - add the exception you want to catch after except
+        is_finished = True
+    except ValueError:
         print("Please enter a valid integer.")
+# PyCharm will probably give you a warning that result "may be undefined". This is safe to ignore.
 print("Valid result is:", result)
