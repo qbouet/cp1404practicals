24c61cd669bdc4cf8824e4afe144d4feaf82eb9b
qbouet <quentin.bouet@my.jcu.edu.au>
2023-10-05 12:31:02 +1000

    Update to print data through function

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,12 +8,13 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    print_data(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    data = []
     for line in input_file:
         print(line)  # See what a line looks like
         print(repr(line))  # See what a line really looks like
@@ -23,7 +24,15 @@ def get_data():
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
         print("----------")
+        data.append(parts)
     input_file.close()
+    return data
+
+
+def print_data(data):
+    """Print data formatted like: {subject} is taught by {name} and has {number_of_students} students."""
+    for record in data:
+        print(f"{record[0]} is taught by {record[1]:12} and has {record[2]:3} students")
 
 
 main()
