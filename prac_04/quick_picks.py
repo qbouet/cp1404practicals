f92e6dd7e6ace0fb62563b6933ab7d75e934c489
qbouet <quentin.bouet@my.jcu.edu.au>
2023-10-06 16:02:27 +1000

    Add quick picks program

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,8 @@
+"""Write a program that asks the user how many "quick picks" they wish to generate.
+The program then generates that many lines of output.
+Each line consists of 6 random numbers between 1 and 45."""
+
+def main():
+    number_of_quick_picks = input("How many quick picks? ")
+
+main()
