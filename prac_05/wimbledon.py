55acd95cdcbfc5e1dc3155b93cfa5e1241d81da1
qbouet <quentin.bouet@my.jcu.edu.au>
2023-10-13 23:51:41 +1000

    Refactor prac 5 wimbledon program

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -9,27 +9,47 @@ CHAMPION_INDEX = 2
 
 
 def main():
+    """Read csv file and display details about Wimbledon champions and countries."""
+    records = get_records(FILE_NAME)
+    champion_to_win, countries = process_records(records)
+    display_results(champion_to_win, countries)
+
+
+def display_results(champion_to_win, countries):
+    """Display champions and countries"""
+    print("Wimbledon Champions: ")
+    for champion, number_of_wins in champion_to_win.items():
+        print(champion, number_of_wins)
+    print(f"\nThese {len(countries)} countries have won Wimbledon:")
+    print(", ".join(sorted(countries)))
+
+
+def process_records(records):
+    """Create dictionary of champions to wins and set of countries from records."""
     countries = set()
     champions = []
     champion_to_win = {}
-    with open(FILE_NAME, "r", encoding="utf-8-sig") as in_file:
-        in_file.readline()  # ignore header
-        for line in in_file:
-            parts = line.strip().split(',')
-            champion = parts[2]
-            champions.append(champion)
-            country = parts[1]
-            countries.add(country)
-
+    for record in records:
+        champion = record[CHAMPION_INDEX]
+        champions.append(champion)
+        country = record[COUNTRY_INDEX]
+        countries.add(country)
     for champion in champions:
         number_of_wins = champions.count(champion)
         champion_to_win[champion] = number_of_wins
+    return champion_to_win, countries
 
-    for champion, number_of_wins in champion_to_win.items():
-        print(f"{champion} {number_of_wins}")
 
-    print(f"\nThese {len(countries)} countries have won Wimbledon:")
-    print(", ".join(sorted(countries)))
+def get_records(file_name):
+    """Get records from file in list of lists form."""
+    records = []
+    with open(file_name, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()  # ignore header
+        for line in in_file:
+            parts = line.strip().split(',')
+            records.append(parts)
+
+    return records
 
 
 main()
