23859edc0079ab86a54681f8c2f4baa55008ff94
qbouet <quentin.bouet@my.jcu.edu.au>
2023-10-15 20:15:38 +1000

    Add missing code to Prac 5 state names program

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -5,7 +5,9 @@ State names in a dictionary
 
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-print(CODE_TO_NAME)
+
+for state_code in CODE_TO_NAME:
+    print(f"{state_code:<3} is {CODE_TO_NAME[state_code]}")
 
 state_code = input("Enter short state: ").upper()
 
