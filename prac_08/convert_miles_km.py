5e60ac701231c66cf20cb782dcb9a8e652172b75
qbouet <quentin.bouet@my.jcu.edu.au>
2023-11-09 21:36:52 +1000

    Complete Stage 1 for Do-from-scratch Exercises for Prac 8

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -5,17 +5,31 @@ Quentin Bouet, IT@JCU
 """
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
 
 __author__ = 'Quentin Bouet'
+
+MILES_TO_KM = 1.60934
 
 
 class MilesConvertingApp(App):
     """ MilesConvertingApp is a Kivy App for converting miles to kilometres """
+    message = StringProperty()
+
     def build(self):
         """ build the Kivy app from the kv file """
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
+        self.message = "Type number in the field & press Enter"
         return self.root
+
+    def handle_calculate(self):
+        """Handle calculations from the text input by updating the model from the view."""
+        self.message = str(float(self.root.ids.input_number.text) * MILES_TO_KM)
+
+    def handle_increment(self, increment):
+        """Handle increments to the text input by updating the model from the view."""
+        self.root.ids.input_number.text = str(float(self.root.ids.input_number.text) + increment)
 
 
 MilesConvertingApp().run()
