2bff98b9217429a64dd37d523d42f6807341556c
qbouet <quentin.bouet@my.jcu.edu.au>
2023-11-12 14:33:06 +1000

    Add docstrings to box layout demo for Prac 8

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -1,14 +1,23 @@
+"""
+CP1404 Week 11 Workshop - Box Layout Demo
+Quentin Bouet, IT@JCU
+12/11/2023
+"""
+
 from kivy.app import App
 from kivy.lang import Builder
 
 
 class BoxLayoutDemo(App):
+    """Main program - Kivy app to demo box layout."""
     def build(self):
+        """Build the Kivy GUI."""
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
     def handle_greet(self):
+        """Handle greeting of user"""
         print("test")
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
