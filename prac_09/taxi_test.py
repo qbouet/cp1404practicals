39fe906f31cfb1cb7c514f3df040f4f22c939277
qbouet <quentin.bouet@my.jcu.edu.au>
2023-11-16 11:12:03 +1000

    Complete first part of Walkthrough Example for Prac 9

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,14 @@
+"""Taxi Test"""
+from taxi import Taxi
+
+my_taxi = Taxi("Prius 1", 100, 1.23)
+print(my_taxi)
+
+my_taxi.drive(40)
+print(f"Name: {my_taxi.name} Fuel: {my_taxi.fuel} Price per km: ${my_taxi.price_per_km}")
+print(f"Fare: {my_taxi.get_fare()}")
+
+my_taxi.start_fare()
+my_taxi.drive(100)
+print(f"Name: {my_taxi.name} Fuel: {my_taxi.fuel} Price per km: ${my_taxi.price_per_km}")
+print(f"Fare: {my_taxi.get_fare()}")
